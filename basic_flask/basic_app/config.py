21fa1ebad3d5aa0befd7be1c2c11318d45de4b6b
Michael Theut <michaeltheut13@gmail.com>
2019-11-19 20:22:23 -0800

    yolo

--- basic_flask/basic_app/config.py
+++ basic_flask/basic_app/config.py
@@ -8,3 +8,6 @@ CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 CELERY_BROKER_URL = "redis://redis:6379/0"
 CELERY_ALWAYS_EAGER = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# docker-compose run db psql -h db -U hello_flask -d hello_flask_dev
